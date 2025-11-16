f010f4159e5b4bc8f0e9aa1e5f40fb587e6d8d59
Feli <fi.huerta.d@gmail.com>
2025-11-15 22:48:50 -0300

    agregamos *json al repo

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 const app = express();
 import jwt from "jsonwebtoken";
 import cors from "cors";
-const port = 3000;
+const port = process.env.PORT || 3000;
 import {
     pool,
 } from "./querys.js";
