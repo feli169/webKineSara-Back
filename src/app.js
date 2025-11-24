45d218618333d7ae0ed8639ec0dc6d0626a0c33d
Feli <fi.huerta.d@gmail.com>
2025-11-23 23:20:59 -0300

    db.js eliminados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+
 import { addUser, obtenerServicios, loginUser } from "./querys.js";
 
 
--- src/querys.js
+++ src/querys.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-import { pool } from "./db.js";
 
 dotenv.config();
+
+import pkg from "pg";
+const { Pool } = pkg;
+import bcrypt from "bcryptjs";
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false },
+});
 
 export const addUser = async (user) => {
   const { Nombre, Apellido, Email, Telefono, Pass } = user;
