01b9e618943fed5f04846f11ac9157b8101a62cd
Feli <fi.huerta.d@gmail.com>
2025-11-24 21:00:16 -0300

    add pool app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { pool } from "./querys.js";
+
 
 import { addUser, obtenerServicios, loginUser } from "./querys.js";
 
